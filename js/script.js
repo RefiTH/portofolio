bbb898ecff04bea5dd495decc2f8281631728166
Refi Tri Hidayatulah <47969645+RefiTH@users.noreply.github.com>
2021-08-21 17:02:57 +0700

    upload file web portofolio
    upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const txtElement = ['Freelancer', 'Web Developer', 'Software Engginer'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function mengetik() {
+    if (count == txtElement.length) {
+        count = 0;
+    }
+    currentTxt = txtElement[count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-mengetik').textContent = words;
+
+    if (words.length == currentTxt.length) {
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(mengetik, 500);
+})();
